72624fc6897397fd0ad743da5ce5e68988d1b0a4
zhangyi1237654 <329757823@qq.com>
2017-06-25 06:50:57 +0800

    1
    1

--- src/com/zy/db/DBUtil.java
+++ src/com/zy/db/DBUtil.java
@@ -2,9 +2,7 @@ package com.zy.db;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 /**
 * @author zy007
